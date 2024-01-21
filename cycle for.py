80ca5b05da6671b0d698a4e8c2a85f0afded3ffe
sdmit <sdmitriyyu@gmail.com>
2024-01-21 16:47:57 +0300

    Цикл for

---
+++ cycle for.py
@@ -0,0 +1,7 @@
+my_garage = ['BMW', 'MB', 'LADA', 'KIA', 'HONDA']
+cars_count = 0
+for car in my_garage:
+    cars_count += 10
+    print('Я езжу на машине марки', car)
+    print('Машин сегодня', cars_count)
+
