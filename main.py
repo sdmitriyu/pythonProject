f09dce90f2420f93c56f2e478617f77e16434f07
Дмитрий Соловьёв <sdmitriyyu@gmail.com>
2024-01-30 20:55:34 +0300

    Функци

--- main.py
+++ main.py
@@ -3,12 +3,15 @@ def print_params(a=1, b='строка', c=True):
     print(a, b)
     print(b, c)
     print(a, c)
+
 print_params()
-#def print_params(b=25, c=[1, 2, 3]):
-#    print(print_params)
-# print_params()
-f = 12
-lst = [1, 'two', f]
-slr = {1:'цифра', 'строка':'буквы', True:'переменная'}
-print_params = (*lst, **slr)
-print(print_params)
+
+def print_params(b=25, c=[1, 2, 3]):
+    print(b, c)
+
+print_params()
+
+lst = [1, 'строка', True]
+slr = {'a': 1, 'b': 'строка', 'c': True}
+print_params(*lst)
+print_params(**slr)
